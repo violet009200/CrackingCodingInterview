5c7c2fef9e1a51e958aa0fc152343653c206427f
violet009200 <violet009200@gmail.com>
2020-04-13 17:38:36 -0500

    First commit

---
+++ CrackingCoding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrackingCoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
