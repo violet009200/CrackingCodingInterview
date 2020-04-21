0e1b2f9a7fce4bdff9b8dfc4e2342e3cfc061e61
violet009200 <violet009200@gmail.com>
2020-04-21 18:06:28 -0500

    1.8 Zero Matrix

---
+++ CrackingCoding/Chap1_ArraysAndStrings/Chap1_ZeroMatrix.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chap1_ArraysAndStrings
+{
+    public class Chap1_ZeroMatrix
+    {
+        /*
+         * space: O(N)
+         * time: O(N*M)
+         */
+        public int[,] ZeroMatrix(int[,] m)
+        {
+            List<int> rows = new List<int>();
+            List<int> cols = new List<int>();
+
+
+            for(int i = 0; i < m.GetLength(0); i++)
+            {
+                for(int j=0; j<m.GetLength(1); j++)
+                {
+                    if(m[i,j] == 0)
+                    {
+                        rows.Add(i);
+                        cols.Add(j);
+                    }
+                }
+            }
+
+            foreach(int r in rows)
+            {
+                for(int i = 0; i < m.GetLength(1); i++)
+                {
+                    m[r,i] = 0;
+                }
+            }
+
+            foreach (int c in cols)
+            {
+                for (int i = 0; i < m.GetLength(0); i++)
+                {
+                    m[i,c] = 0;
+                }
+            }
+
+            return m;
+        }
+
+        public int[,] CreateMatrix(int m, int n, params int[] input)
+        {
+            if (input == null) throw new ArgumentException("matrix is null");
+
+            int[,] result = new int[m, n];
+            int cnt = 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    result[i, j] = input[cnt++];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Test/Chap1_ArraysAndStrings/Chap1_ZeroMatrixTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Xunit;
+
+namespace Chap1_ArraysAndStrings
+{
+    public class Chap1_ZeroMatrixTest
+    {
+        //invalid input
+        [Fact]
+        public void MatrixIsNull()
+        {
+            Chap1_ZeroMatrix m = new Chap1_ZeroMatrix();
+            Assert.Throws<ArgumentException>(() => m.CreateMatrix(0 ,0, null));
+        }
+
+        //matrix has one elem
+        [Fact]
+        public void OneElement()
+        {
+            Chap1_ZeroMatrix m = new Chap1_ZeroMatrix();
+            int[,] expect = m.CreateMatrix(1,1,1);
+            int[,] result = m.ZeroMatrix(expect);
+
+            Assert.Equal(expect, result);
+        }
+
+        //matrix without 0
+        [Fact]
+        public void WithoOutZero()
+        {
+            Chap1_ZeroMatrix m = new Chap1_ZeroMatrix();
+            int[,] expect = m.CreateMatrix(2, 2, 1, 2, 3, 4);
+            int[,] result = m.ZeroMatrix(expect);
+
+            Assert.Equal(expect, result);
+        }
+
+        //Matrix with 0
+        [Fact]
+        public void WithZeros()
+        {
+            Chap1_ZeroMatrix m = new Chap1_ZeroMatrix();
+            int[,] input = m.CreateMatrix(3, 3, 1, 2, 0, 3, 0, 4, 5, 6, 7);
+            int[,] result = m.ZeroMatrix(input);
+            int[,] expect = m.CreateMatrix(3, 3, 0, 0, 0, 0, 0, 0, 5, 0, 0);
+
+            int[,] input1 = m.CreateMatrix(3, 3, 1,2,3,4,0,5,6,7,8);
+            int[,] result1 = m.ZeroMatrix(input1);
+            int[,] expect1 = m.CreateMatrix(3, 3, 1,0,3,0,0,0,6,0,8);
+
+            Assert.Equal(expect, result);
+            Assert.Equal(expect1, result1);
+
+        }
+
+    }
+}
