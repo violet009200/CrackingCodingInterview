1c39eb5082681595e666c359b492a58a2be47d45
violet009200 <violet009200@gmail.com>
2020-04-21 16:53:33 -0500

    1.7 Rotate Matrix

---
+++ CrackingCoding/Chap1_ArraysAndStrings/Chap1_RotateMatrix.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Chap1_ArraysAndStrings
+{
+    public class Chap1_RotateMatrix
+    {
+        /*
+         * Runtime: o(n^2)
+         * space:o(n^2)
+         */
+        public int[,] RotateMatrix(int[,] matrix)
+        {
+            int size = matrix.GetLength(0);
+
+            //create new Array
+            int[,] result = new int[size, size];
+
+            for(int i = 0; i < size; i++)
+            {
+                for(int j=0; j<size; j++)
+                {
+                    result[j, size-1-i] = matrix[i, j];
+                }
+            }
+            return result;
+        }
+
+        /*
+         * runtime: o(n^2)
+         */
+        public int[,] RotateMatrix_inPlace(int[,] matrix)
+        {
+            int size = matrix.GetLength(0);
+
+            if (size == 1) return matrix;
+            //swap matrix top with left, bottom, right
+            int curLevel = 0;
+            int totalLevel = size / 2;
+            int last = size - 1;
+
+            while (curLevel < totalLevel)
+            {
+                for (int i = curLevel; i < last; i++)
+                {
+                    //left
+                    int top = matrix[curLevel, i];                   
+                    matrix[curLevel, i] = matrix[i, last];
+                    matrix[i, last] = top;
+
+                    //bottom
+                    top = matrix[curLevel, i];
+                    //top = matrix[last, last - i + curLevel];
+                    matrix[curLevel, i] = matrix[last, last - i + curLevel];
+                    matrix[last, last - i + curLevel] = top;
+
+                    //right
+                    top = matrix[curLevel, i];
+                    matrix[curLevel, i] = matrix[last - i + curLevel, curLevel];
+                    matrix[last - i + curLevel, curLevel] = top;
+
+                }
+                curLevel++;
+                last--;
+            }
+
+            return matrix;
+
+        }
+
+        public int[,] CreateMatrix(params int[] input)
+        {
+            if (input == null) throw new ArgumentException("matrix is null");
+            int size = getSizeMatrix(input);
+
+            int[,] result = new int[size,size];
+            int cnt = 0;
+
+            for(int i = 0; i<size; i++)
+            {
+                for(int j=0; j<size; j++)
+                {
+                    result[i,j] = input[cnt++];
+                }
+            }
+
+            return result;
+        }
+
+        private int getSizeMatrix(params int[] input)
+        {
+            double size = Math.Sqrt(input.Length);
+
+            if(size%1 != 0)
+            {
+               throw new ArgumentException("matrix is not square");
+            }
+
+            return (int)size;
+        }
+    }
+}
---
+++ Test/Chap1_ArraysAndStrings/Chap1_RotateMatrixTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Xunit;
+
+namespace Chap1_ArraysAndStrings
+{
+    public class Chap1_RotateMatrixTest
+    {
+        //matrix is null
+        [Fact]
+        public void MatrixIsNull()
+        {
+            Chap1_RotateMatrix matrix = new Chap1_RotateMatrix();
+            Assert.Throws<ArgumentException>(() =>  matrix.CreateMatrix(null));
+        }
+
+        //Matrix is not squre
+        [Fact]
+        public void MatrixIsNotSquare()
+        {
+            Chap1_RotateMatrix matrix = new Chap1_RotateMatrix();
+            Assert.Throws<ArgumentException>(() => matrix.CreateMatrix(1, 2, 3, 4, 5, 6));
+        }
+        //Matrix 1x1
+        [Fact]
+        public void OneByOneMatrix()
+        {
+            Chap1_RotateMatrix matrix = new Chap1_RotateMatrix();
+            int[,] input = matrix.RotateMatrix(matrix.CreateMatrix(1));
+            int[,] result = matrix.CreateMatrix(1);
+
+            int[,] input2 = matrix.RotateMatrix_inPlace(matrix.CreateMatrix(1));
+
+            Assert.Equal(result, input);
+
+            Assert.Equal(result, input2);
+        }
+        //Matrix 2x2
+        [Fact]
+        public void TwoByTwoMatrix()
+        {
+            Chap1_RotateMatrix m = new Chap1_RotateMatrix();
+            int[,] input = m.RotateMatrix(m.CreateMatrix(1,2,3,4));
+            int[,] result = m.CreateMatrix(3,1,4,2);
+
+            int[,] input2 = m.RotateMatrix_inPlace(m.CreateMatrix(1,2,3,4));
+
+            Assert.Equal(result, input);
+            Assert.Equal(result, input2);
+        }
+        //Matrix 3x3
+        [Fact]
+        public void ThreeByThreeMatrix()
+        {
+            Chap1_RotateMatrix m = new Chap1_RotateMatrix();
+            int[,] input = m.RotateMatrix(m.CreateMatrix(1, 2, 3, 4,5,6,7,8,9));
+            int[,] result = m.CreateMatrix(7,4,1,8,5,2,9,6,3);
+
+            int[,] input2 = m.RotateMatrix_inPlace(m.CreateMatrix(1, 2, 3, 4,5,6,7,8,9));
+
+            Assert.Equal(result, input);
+            Assert.Equal(result, input2);
+
+        }
+    }
+}
