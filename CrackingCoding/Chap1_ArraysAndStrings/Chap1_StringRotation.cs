e6dff2cee4c71e5617fcc92bc0e1a1cc807d72d3
violet009200 <violet009200@gmail.com>
2020-04-21 18:19:59 -0500

    i.9 String Rotation

---
+++ CrackingCoding/Chap1_ArraysAndStrings/Chap1_StringRotation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Chap1_ArraysAndStrings
+{
+    public class Chap1_StringRotation
+    {
+        public bool isSubstring(string input, string sub)
+        {
+            return input.IndexOf(sub) >= 0 ? true : false;
+        }
+
+        public bool IsRotation(string input, string compare)
+        {
+            if (input.Length != compare.Length) return false;
+
+            var result = compare + compare;
+
+            return isSubstring(result, input);
+        }
+    }
+}
---
+++ Test/Chap1_ArraysAndStrings/Chap1_StringRotationTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+
+namespace Chap1_ArraysAndStrings
+{
+    public class Chap1_StringRotationTest
+    {
+        //Test isSubstring
+        [Fact]
+        public void CheckIsSubstring()
+        {
+            Chap1_StringRotation s = new Chap1_StringRotation();
+
+            Assert.True(s.isSubstring("abcd", "bc"));
+        }
+
+        [Fact]
+        public void ShouldReturnFalse()
+        {
+            Chap1_StringRotation s = new Chap1_StringRotation();
+            bool result = s.IsRotation("water", "waert");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ShouldReturnTrue()
+        {
+            Chap1_StringRotation s = new Chap1_StringRotation();
+            bool result = s.IsRotation("waterbottle", "erbottlewat");
+            Assert.True(result);
+        }
+    }
+}
