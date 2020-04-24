7641ae029adc8f0e86b0192abef001963b15fdb9
violet009200 <violet009200@gmail.com>
2020-04-24 12:25:49 -0500

    2.2 Return Kth to Last

---
+++ CrackingCoding/Chap2_LinkedLists/GetKthToLast.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chap2_LinkedLists
+{
+    public class GetKthToLast
+    {
+       public int GetKth_WithSizeInput(LinkedList<int> input, int k)
+        {
+            int size = input.Count;
+            if (size < k) throw new ArgumentException("k is bigger than size");
+            if (size == k) return input.First.Value;
+
+            int cnt = size - k;
+
+            var head = input.First;
+
+            while(cnt > 0)
+            {
+                head = head.Next;
+                cnt--;
+            }
+
+            return head.Value;
+        }
+
+        /*
+         * time O(n)
+         * space O(1)
+         */
+        public int GetKth_WithOutSizeInput(LinkedList<int> input, int k)
+        {
+            var result = input.First;
+            var runner = input.First;
+
+            k = k - 1;
+
+            while(k > 0)
+            {
+                if (runner.Next != null)
+                {
+                    runner = runner.Next;
+                    k--;
+                }
+                else throw new ArgumentException("K is bigger than size of input");
+            }
+
+            while(runner.Next != null)
+            {
+                runner = runner.Next;
+                result = result.Next;
+            }
+
+            return result.Value;
+        }
+    }
+}
---
+++ Test/Chap2_LinkedLists/GetKthToLastTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Xunit;
+using Helper;
+using System.Collections.Generic;
+
+namespace Chap2_LinkedLists
+{
+    public class GetKthToLastTest
+    {   
+
+        //k is bigger than size of input list
+        [Fact]
+        public void KIsBiggerThanInputSize()
+        {
+            LinkedListHelper<int> helper = new LinkedListHelper<int>();
+            LinkedList<int> input = helper.CreateLinkedList(1,2);
+
+            GetKthToLast getKth = new GetKthToLast();
+            
+            Assert.Throws<ArgumentException>(() => getKth.GetKth_WithSizeInput(input, 4));
+            Assert.Throws<ArgumentException>(() => getKth.GetKth_WithOutSizeInput(input, 4));
+
+        }
+
+        [Fact]
+        public void KIsSameAsInputSize()
+        {
+            LinkedListHelper<int> helper = new LinkedListHelper<int>();
+            LinkedList<int> input = helper.CreateLinkedList(1, 2, 3, 4, 5);
+
+            GetKthToLast getKth = new GetKthToLast();
+            int k = getKth.GetKth_WithSizeInput(input, 5);
+            int r = getKth.GetKth_WithOutSizeInput(input, 5);
+
+            Assert.Equal(1, r);
+
+            Assert.Equal(1, k);
+        }
+
+        [Fact]
+        public void BasicTest()
+        {
+            LinkedListHelper<int> helper = new LinkedListHelper<int>();
+            LinkedList<int> input = helper.CreateLinkedList(1, 2, 3, 4, 5);
+
+            GetKthToLast getKth = new GetKthToLast();
+            int k = getKth.GetKth_WithSizeInput(input, 2);
+
+            int r = getKth.GetKth_WithOutSizeInput(input, 2);
+
+            Assert.Equal(4, k);
+            Assert.Equal(4, r);
+        }
+
+    }
+}
