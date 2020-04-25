1c9f03ba56b733df8d135733318b2e6e3e94ec1e
violet009200 <violet009200@gmail.com>
2020-04-25 16:37:47 -0500

    2.5 Sum Lists

---
+++ CrackingCoding/Chap2_LinkedLists/SumLists.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Helper;
+
+namespace Chap2_LinkedLists
+{
+    public class SumLists
+    {
+        /*
+         * time: O(n)
+         * space O(n)
+         */
+        public Node<int> SumList_reverse(Node<int> head1, Node<int> head2)
+        {
+            if (head1 == null && head2 == null) throw new ArgumentNullException("input list is null");
+
+            Node<int> result = null;
+            Node<int> resultHead = null;
+
+            int val = 0;
+            int carry = 0;
+            int digit = 0;
+
+            while(head1!= null || head2 != null)
+            {
+                int data1 = 0;
+                int data2 = 0;
+
+                if (head1 != null) data1 = head1.Data;
+                if (head2 != null) data2 = head2.Data;
+
+                val = data1 + data2 + carry;
+                digit = val % 10;
+                carry = val / 10;
+
+                if(resultHead == null)
+                {
+                    resultHead = new Node<int>(digit);
+                    result = resultHead;
+                }
+                else
+                {
+                    result.Next = new Node<int>(digit);
+                    result = result.Next;
+                }
+
+                head1 = head1?.Next;
+                head2 = head2?.Next;
+            }
+
+            return resultHead;
+        }
+
+    }
+}
---
+++ Test/Chap2_LinkedLists/SumListsTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xunit;
+using Helper;
+
+namespace Chap2_LinkedLists
+{
+    public class SumListsTest
+    {
+        [Fact]
+        public void ListHasSameLength()
+        {
+            LinkedListHelper<int> helper = new LinkedListHelper<int>();
+            var input1 = helper.CreateLinkedList_Node(7, 1, 6);
+            var input2 = helper.CreateLinkedList_Node(5, 9, 2);
+
+            SumLists sum = new SumLists();
+
+            var result = sum.SumList_reverse(input1, input2);
+
+            string resultVal = helper.PrintLinkedList(result);
+
+            Assert.Equal("2 1 9", resultVal);
+        }
+
+        [Fact]
+        public void ListHasDifferentLength()
+        {
+            LinkedListHelper<int> helper = new LinkedListHelper<int>();
+            var input1 = helper.CreateLinkedList_Node(7, 1, 6);
+            var input2 = helper.CreateLinkedList_Node(5, 9);
+
+            SumLists sum = new SumLists();
+
+            var result = sum.SumList_reverse(input1, input2);
+
+            string resultVal = helper.PrintLinkedList(result);
+
+            Assert.Equal("2 1 7", resultVal);
+        }
+    }
+}
