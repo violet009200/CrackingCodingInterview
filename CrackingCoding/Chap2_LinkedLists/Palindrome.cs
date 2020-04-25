a4edb8dd156c63fdb6670f1cdb0f746346b81b6b
violet009200 <violet009200@gmail.com>
2020-04-25 16:52:11 -0500

    2.6 Palindrome

---
+++ CrackingCoding/Chap2_LinkedLists/Palindrome.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Helper;
+
+namespace Chap2_LinkedLists
+{
+    public class Palindrome
+    {
+        public bool isPalindrome(Node<int> head)
+        {
+            if (head == null) throw new ArgumentNullException("node is null");
+
+            var reversed = Reverse(head);
+
+            while(head != null)
+            {
+                if (head.Data == reversed.Data)
+                {
+                    head = head.Next;
+                    reversed = reversed.Next;
+                }
+                else return false;
+            }
+
+            return true;
+        }
+
+        public Node<int> Reverse(Node<int> head)
+        {
+            if (head == null) throw new ArgumentNullException("node is null");
+
+            Node<int> reverseHead = null;
+
+            while(head != null)
+            {
+                Node<int> tmp = new Node<int>(head.Data); // clone
+                tmp.Next = reverseHead;
+                reverseHead = tmp;
+                head = head.Next;
+            }
+            return reverseHead;
+        }
+    }
+}
---
+++ Test/Chap2_LinkedLists/PalindromeTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+using Helper;
+
+namespace Chap2_LinkedLists
+{
+    public class PalindromeTest
+    {
+       [Fact]
+       public void BasicTest()
+        {
+            LinkedListHelper<int> helper = new LinkedListHelper<int>();
+            var inputHead = helper.CreateLinkedList_Node(0, 1, 2, 1, 0);
+
+            Palindrome p = new Palindrome();
+            var result = p.isPalindrome(inputHead);
+
+            var inputHead2 = helper.CreateLinkedList_Node(0, 1, 2, 3, 4);
+            var result2 = p.isPalindrome(inputHead2);
+
+            Assert.True(result);
+            Assert.False(result2);
+        }
+    }
+}
