362192f2ecb4ae2f10a4d943ad1f045e72c3bb91
violet009200 <violet009200@gmail.com>
2020-04-27 10:54:14 -0500

    2.7 Intersection

---
+++ CrackingCoding/Chap2_LinkedLists/Intersection.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Helper;
+
+namespace Chap2_LinkedLists
+{
+    public class Intersection
+    {
+        public Node<int> FindIntersection(Node<int> list1, Node<int> list2)
+        {
+            if (list1 == null || list2 == null) return null;
+
+            int list1Size = GetLength(list1);
+            int list2Size = GetLength(list2);
+
+            if (!IsIntersection(list1, list2)) return null;
+
+            if(list1Size > list2Size)
+            {
+                for(int i = 0; i< list1Size - list2Size; i++)
+                {
+                    list1 = list1.Next;
+                }
+            }
+            else
+            {
+                for(int i = 0; i<list2Size-list1Size; i++)
+                {
+                    list2 = list2.Next;
+                }
+            }
+
+            while(list1.Next != null)
+            {
+                if (ReferenceEquals(list1, list2)) return list1;
+                else
+                {
+                    list1 = list1.Next;
+                    list2 = list2.Next;
+                }
+            }
+
+            return null;
+
+        }
+
+        public bool IsIntersection(Node<int> list1, Node<int> list2)
+        {
+            return GetTail(list1) == GetTail(list2) ? true : false;
+        }
+
+        public Node<int> GetTail(Node<int> list)
+        {
+            var cur = list;
+            while(cur.Next != null)
+            {
+                cur = cur.Next;
+            }
+
+            return cur;
+        }
+
+        public int GetLength(Node<int> list)
+        {
+            int size = 0;
+            while(list.Next != null)
+            {
+                size++;
+                list = list.Next;
+            }
+
+            return size;
+        }
+
+        public void CreateIntersection(Node<int> list1, Node<int> list2, Node<int> intersection)
+        {
+            while (list1.Next != null) list1 = list1.Next;
+            list1.Next = intersection;
+
+            while (list2.Next != null) list2 = list2.Next;
+            list2.Next = intersection;
+        }
+    }
+}
---
+++ Test/Chap2_LinkedLists/IntetsectionTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Xunit;
+using Helper;
+
+namespace Chap2_LinkedLists
+{
+    public class IntetsectionTest
+    {
+        [Fact]
+        public void FindIntersectioWithNoIntersection()
+        {
+            Intersection i = new Intersection();
+            LinkedListHelper<int> helper = new LinkedListHelper<int>();
+            Node<int> list1 = helper.CreateLinkedList_Node(1, 2, 3, 4);
+            Node<int> list2 = helper.CreateLinkedList_Node(5, 6, 7, 8);
+
+            Node<int> result = i.FindIntersection(list1, list2);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void FindIntersectionWithIntersection()
+        {
+            Intersection i = new Intersection();
+            LinkedListHelper<int> helper = new LinkedListHelper<int>();
+            Node<int> list1 = helper.CreateLinkedList_Node(1, 2, 3, 4);
+            Node<int> list2 = helper.CreateLinkedList_Node(5, 6, 7, 8);
+
+            Node<int> intersection = helper.CreateLinkedList_Node(9, 10, 11);
+
+            i.CreateIntersection(list1, list2, intersection);
+
+            Node<int> result = i.FindIntersection(list1, list2);
+
+            Assert.Equal("9", result.Data.ToString());
+        }
+
+        [Fact]
+        public void IsIntersectionWithNoIntersection()
+        {
+            Intersection i = new Intersection();
+            LinkedListHelper<int> helper = new LinkedListHelper<int>();
+            Node<int> list1 = helper.CreateLinkedList_Node(1, 2, 3, 4);
+            Node<int> list2 = helper.CreateLinkedList_Node(5, 6, 7, 8);
+
+            bool result = i.IsIntersection(list1, list2);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IstIntersectionWithIntersection()
+        {
+            Intersection i = new Intersection();
+            LinkedListHelper<int> helper = new LinkedListHelper<int>();
+            Node<int> list1 = helper.CreateLinkedList_Node(1, 2, 3, 4);
+            Node<int> list2 = helper.CreateLinkedList_Node(5, 6, 7, 8);
+
+            Node<int> intersection = helper.CreateLinkedList_Node(9, 10, 11);
+
+            i.CreateIntersection(list1, list2, intersection);
+
+            bool result = i.IsIntersection(list1, list2);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TestIntersection()
+        {
+            Intersection i = new Intersection();
+            LinkedListHelper<int> helper = new LinkedListHelper<int>();
+            Node<int> list1 = helper.CreateLinkedList_Node(1, 2, 3, 4);
+            Node<int> list2 = helper.CreateLinkedList_Node(5, 6, 7, 8);
+
+            Node<int> intersection = helper.CreateLinkedList_Node(9, 10, 11);
+
+            i.CreateIntersection(list1, list2, intersection);
+
+            var list1Result = helper.PrintLinkedList(list1);
+            var list2Result = helper.PrintLinkedList(list2);
+
+            Assert.Equal("1 2 3 4 9 10 11", list1Result);
+            Assert.Equal("5 6 7 8 9 10 11", list2Result);
+        }
+    }
+}
